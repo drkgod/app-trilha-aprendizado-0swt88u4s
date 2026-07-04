56576f229cc99b403fb027899b2249be650084b2
Skip <skip@usecurling.com>
2026-07-03 22:35:19 -0300

    Fix login endpoint to use backend URL

--- src/lib/pocketbase/client.ts
+++ src/lib/pocketbase/client.ts
@@ -1,6 +1,10 @@
 import PocketBase from 'pocketbase'
 
-const pb = new PocketBase(import.meta.env.VITE_POCKETBASE_URL)
+const BACKEND_URL =
+  import.meta.env.VITE_POCKETBASE_URL ||
+  'https://app-trilha-aprendizado-ba831.shrd00.internal.goskip.dev'
+
+const pb = new PocketBase(BACKEND_URL)
 pb.autoCancellation(false)
 
 export default pb
